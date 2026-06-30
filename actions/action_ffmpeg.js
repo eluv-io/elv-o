362350b4f7dd4b4f9fe44c1333b8ca94c3b6d8a7
Marc-Olivier Lusinchi <marc@eluv.io>
2026-06-30 14:57:36 +0200

    Adds support for custom execution priority (using nice) and option to execute with variable number of files

--- actions/action_ffmpeg.js
+++ actions/action_ffmpeg.js
@@ -6,19 +6,37 @@ const { execSync, spawn } = require('child_process');
 class ElvOActionFfmpeg extends ElvOAction  {
     
     Parameters() {
-        return {"parameters": {"aws_s3_inputs": {type: "boolean"}, "command_line_options": {type:"string", required: true}, "number_of_input_files":{type: "numeric", required: false}, "variables": {type:"object", required: false}}};
+        return {
+            parameters: {
+                aws_s3_inputs: {type: "boolean"}, 
+                command_line_options: {type:"string", required: false, default: null}, 
+                number_of_input_files: {type: "numeric", required: false}, 
+                variable_number_of_input_files: {type: "boolean", required: false, default: false},
+                variable_number_of_input_files: {type: "boolean", required: false, default: false},
+                variables: {type:"object", required: false}
+            }
+        };
     };
     
     IOs(parameters) {
-        let inputs = this.parseDynamicVariables(parameters.command_line_options, parameters.variables);
-        if (parameters.number_of_input_files &&  (parameters.number_of_input_files != 1)) {
-            for (let i=1; i <= parameters.number_of_input_files; i++) {
-                inputs["input_file_path_"+i] = {type: "file", required: true};
+        let inputs;
+        if (parameters.command_line_options) {     
+             inputs = this.parseDynamicVariables(parameters.command_line_options, parameters.variables);
+        } else {
+            inputs = {command_line_options: "string"};
+        }
+        if (!parameters.variable_number_of_input_files) {
+            if (parameters.number_of_input_files &&  (parameters.number_of_input_files != 1)) {
+                for (let i=1; i <= parameters.number_of_input_files; i++) {
+                    inputs["input_file_path_"+i] = {type: "file", required: true};
+                }
+            } else {
+                if (parameters.number_of_input_files != 0) {
+                    inputs.input_file_path = {type: "file", required: true};
+                }
             }
         } else {
-            if (parameters.number_of_input_files != 0) {
-                inputs.input_file_path = {type: "file", required: true};
-            }
+            inputs.input_file_paths = {type: "array", required: true};
         }
         if (parameters.aws_s3_inputs) {
             inputs.cloud_access_key_id = {type: "string", required:true};
@@ -27,7 +45,13 @@ class ElvOActionFfmpeg extends ElvOAction  {
             inputs.cloud_region = {type: "file", required:true};
         }
         inputs.output_file_path = {type: "file", required: true};
-        return { inputs : inputs, outputs: {"stderr": {type: "string"}, "execution_code": {type:"numeric"}, "output_file_path": {type: "string"}} };
+        inputs.execution_priority = {type: "numeric", required: false, default: null};
+        let outputs = {
+            stderr: {type: "string"}, 
+            execution_code: {type:"numeric"}, 
+            output_file_path: {type: "string"}
+        };
+        return { inputs, outputs };
     };
     
     ActionId() {
@@ -47,20 +71,25 @@ class ElvOActionFfmpeg extends ElvOAction  {
         }
     };
     
-    async Execute(handle, outputs) {
+    async Execute(inputs, outputs) {
         let outputFilePath = this.Payload.inputs.output_file_path;
         //this.Info("command_line_options: " + this.Payload.parameters.command_line_options);
-        let commandLineOptions = await this.expandDynamicVariables(this.Payload.inputs, JSON.stringify(this.Payload.parameters.command_line_options), this.Payload.parameters.variables);
+        let commandLineOptions = inputs.command_line_options || await this.expandDynamicVariables(this.Payload.inputs, JSON.stringify(this.Payload.parameters.command_line_options), this.Payload.parameters.variables);
         //this.Info("expanded command_line_options: " + commandLineOptions);
         let fileInputs = [];
-        let inputFileNum = this.Payload.parameters.hasOwnProperty("number_of_input_files") ?  this.Payload.parameters.number_of_input_files : 1;
+        let inputFileNum;
+        if (!this.Payload.parameters.variable_number_of_input_files) {
+            inputFileNum = this.Payload.parameters.hasOwnProperty("number_of_input_files") ?  this.Payload.parameters.number_of_input_files : 1;
+        } else {
+            inputFileNum = inputs.input_file_paths.length;
+        }
         if (inputFileNum == 1) {
             fileInputs.push("-i");
-            fileInputs.push(this.expandInputFilePath(this.Payload.inputs.input_file_path));
+            fileInputs.push(this.expandInputFilePath(inputs.input_file_path || inputs.input_file_paths[0]));
         } else {
             for (let i=1; i <= inputFileNum; i++) {
                 fileInputs.push("-i");
-                fileInputs.push(this.expandInputFilePath(this.Payload.inputs["input_file_path_"+i]));
+                fileInputs.push(this.expandInputFilePath(inputs["input_file_path_"+i] || inputs.input_file_paths[i -1]));
             }
         }
         /*
@@ -68,37 +97,42 @@ class ElvOActionFfmpeg extends ElvOAction  {
         let enclosed = false;
         let composite;
         for (let c=0; c < components.length; c++) {
-            let component  = components[c];
-            if (!enclosed){
-                let matcher=component.match(/^"(.*)/);
-                if (!matcher) {
-                    if (component) {
-                        args.push(component);
-                    }
-                } else {
-                    let rematch = component.match(/^"(.*)"$/);
-                    if (rematch) {
-                        args.push(rematch[1]);
-                    } else {
-                        enclosed = true;
-                        composite = matcher[1];
-                    }
-                }
-            } else {
-                let matcher = component.match(/^(.*)"$/);
-                if (!matcher) {
-                    composite = composite + " " + component
-                } else {
-                    composite = composite + " " + matcher[1];
-                    enclosed = false;
-                    args.push(composite);
-                    composite = null;
-                }
-            }
+        let component  = components[c];
+        if (!enclosed){
+        let matcher=component.match(/^"(.*)/);
+        if (!matcher) {
+        if (component) {
+        args.push(component);
+        }
+        } else {
+            let rematch = component.match(/^"(.*)"$/);
+        if (rematch) {
+        args.push(rematch[1]);
+        } else {
+            enclosed = true;
+        composite = matcher[1];
+        }
+        }
+        } else {
+            let matcher = component.match(/^(.*)"$/);
+        if (!matcher) {
+        composite = composite + " " + component
+        } else {
+            composite = composite + " " + matcher[1];
+        enclosed = false;
+        args.push(composite);
+        composite = null;
+        }
+        }
         }
         args.push(outputFilePath);
         */
-        let args = ["-c", "ffmpeg " + fileInputs.join(" ") +" "+ commandLineOptions + " \"" + outputFilePath + "\""];
+        let args;
+        if (inputs.execution_priority == null) {
+            args = ["-c", "ffmpeg " + fileInputs.join(" ") +" "+ commandLineOptions + " \"" + outputFilePath + "\""];
+        } else  {            
+            args = ["-c", "nice -n "+inputs.execution_priority+ " ffmpeg " + fileInputs.join(" ") +" "+ commandLineOptions + " \"" + outputFilePath + "\""];
+        }
         
         this.ReportProgress("Command line prepared");
         this.reportProgress("Command line args", args);
@@ -145,12 +179,14 @@ class ElvOActionFfmpeg extends ElvOAction  {
         }
         return 100;
     };
-    static VERSION = "0.0.3";
     static REVISION_HISTORY = {
         "0.0.1": "Initial release",
         "0.0.2": "Uses sh in spawn to allows piped commands in the command line options",
-        "0.0.3": "Handles s3 partial paths"
+        "0.0.3": "Handles s3 partial paths",
+        "0.0.4": "2026-02-04 - ML - Adds support for custom execution priority (using nice)",
+        "0.0.5": "2026-04-29 - ML - Adds option to execute with variable number of files" 
     };
+    static VERSION = "0.0.5";
 }
 
 
@@ -170,15 +206,15 @@ Command specs
 node actions/action_ffprobe.js specs --private-key=0xprivate --verbose --payload='{"parameters" : {"command_line_options": "-b %BABA%",  "variables":{"BABA":  {type: "string",required:"false","default":"ZOB"}}}}'
 Command specs
 {
-    inputs: {
-        BABA: { type: 'string', required: 'false', default: 'ZOB' },
-        input_file: { type: 'file', required: 'true' }
-    },
-    outputs: {
-        results: { type: 'object', format: 'json' },
-        stderr: { type: 'string' },
-        execution_code: { type: 'numeric' }
-    }
+inputs: {
+BABA: { type: 'string', required: 'false', default: 'ZOB' },
+input_file: { type: 'file', required: 'true' }
+},
+outputs: {
+results: { type: 'object', format: 'json' },
+stderr: { type: 'string' },
+execution_code: { type: 'numeric' }
+}
 }
 
 
@@ -190,9 +226,9 @@ Command execute
 node actions/action_ffprobe.js check-status --handle=1611801130236 --private-key=0xprivate --verbose
 Command check-status
 {
-    status: { state: 'complete', progress_message: 'Probing complete' },
-    handle: '1611801130236',
-    execution_node: ''
+status: { state: 'complete', progress_message: 'Probing complete' },
+handle: '1611801130236',
+execution_node: ''
 }
 
 */
