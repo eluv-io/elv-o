90426526433198597b10dac76281afcf8a72506f
Marc-Olivier Lusinchi <marc@eluv.io>
2026-06-30 14:52:39 +0200

    some clean-up

--- actions/action_aws_s3_operation.js
+++ actions/action_aws_s3_operation.js
@@ -59,7 +59,6 @@ class ElvAwsS3Operation extends ElvOAction  {
         }
         if (parameters.action == "UPLOAD_FILE") {
             inputs.s3_file_path = {type: "string", required: false, default: ""};
-            inputs.endpoint_url = {type: "string", required: false, default: null};
             inputs.cloud_region = {type: "string", required: true};   
             inputs.cloud_access_key_id = {type: "string", required: true};
             inputs.cloud_secret_access_key = {type: "password", required: true};
@@ -69,7 +68,6 @@ class ElvAwsS3Operation extends ElvOAction  {
         }
         if (parameters.action == "UPLOAD_FILES") {
             inputs.s3_file_path = {type: "string", required: false, default: ""};
-            inputs.endpoint_url = {type: "string", required: false, default: null};
             inputs.cloud_region = {type: "string", required: true};   
             inputs.cloud_access_key_id = {type: "string", required: true};
             inputs.cloud_secret_access_key = {type: "password", required: true};
@@ -89,7 +87,6 @@ class ElvAwsS3Operation extends ElvOAction  {
         }
         if (parameters.action == "DOWNLOAD_FILE") {
             inputs.s3_file_path = {type: "string", required: true};
-            inputs.endpoint_url = {type: "string", required: false, default: null};
             inputs.cloud_region = {type: "string", required: true};   
             inputs.cloud_access_key_id = {type: "string", required: true};
             inputs.cloud_secret_access_key = {type: "password", required: true};
@@ -316,10 +313,6 @@ class ElvAwsS3Operation extends ElvOAction  {
     async executeUploadFile(inputs, outputs) {
         let s3Path =  (inputs.s3_file_path.match(/^s3:\/\//)) ? inputs.s3_file_path : ("s3://" + path.join(inputs.cloud_bucket, inputs.s3_file_path));
         let args = ["s3", "cp", inputs.local_path, s3Path];
-        if (inputs.endpoint_url && inputs.endpoint_url.length > 0) {
-            args.push("--endpoint-url");
-            args.push(inputs.endpoint_url);
-        }
         let cloudCredentials = {
             AWS_ACCESS_KEY_ID: inputs.cloud_access_key_id,
             AWS_SECRET_ACCESS_KEY: inputs.cloud_secret_access_key,
@@ -439,10 +432,6 @@ class ElvAwsS3Operation extends ElvOAction  {
     async executeDownloadFile(inputs, outputs) {
         let s3Path =  (inputs.s3_file_path.match(/^s3:\/\//)) ? inputs.s3_file_path : ("s3://" + path.join(inputs.cloud_bucket, inputs.s3_file_path));
         let args = ["s3", "cp", s3Path, inputs.local_path];
-        if (inputs.endpoint_url && inputs.endpoint_url.length > 0) {
-            args.push("--endpoint-url");
-            args.push(inputs.endpoint_url);
-        }
         let cloudCredentials = {
             AWS_ACCESS_KEY_ID: inputs.cloud_access_key_id,
             AWS_SECRET_ACCESS_KEY: inputs.cloud_secret_access_key,
@@ -581,7 +570,7 @@ class ElvAwsS3Operation extends ElvOAction  {
         let execCodes = await this.massInitiateGlacierRetrieval(inputs, outputs)
         let codes = Object.values(execCodes);              
         if (codes.includes(ElvOAction.EXECUTION_EXCEPTION)) {
-            this.ReportProgress("At least one item one exception occured while retrieving from Glacier");
+            this.ReportProgress("At least one item one exception occurred while retrieving from Glacier");
             return ElvOAction.EXECUTION_EXCEPTION;
         }
         if (codes.includes(ElvOAction.EXECUTION_COMPLETE)) {
@@ -629,7 +618,7 @@ class ElvAwsS3Operation extends ElvOAction  {
         
         let codes = Object.values(execCodes);              
         if (codes.includes(ElvOAction.EXECUTION_EXCEPTION)) {
-            this.ReportProgress("At least one item one exception occured while retrieving from Glacier");
+            this.ReportProgress("At least one item one exception occurred while retrieving from Glacier");
             return ElvOAction.EXECUTION_EXCEPTION;
         }
         return ElvOAction.EXECUTION_COMPLETE;
@@ -660,7 +649,7 @@ class ElvAwsS3Operation extends ElvOAction  {
         for (let file in execCodes) {
             let execCode = execCodes[file];
             if (execCode == ElvOAction.EXECUTION_EXCEPTION) {
-                this.ReportProgress("At least one item one exception occured while retrieving from Glacier", file);
+                this.ReportProgress("At least one item one exception occurred while retrieving from Glacier", file);
                 return ElvOAction.EXECUTION_EXCEPTION;
             }
             if (execCode == ElvOAction.EXECUTION_COMPLETE) {
@@ -728,7 +717,7 @@ class ElvAwsS3Operation extends ElvOAction  {
                     return ElvOAction.EXECUTION_ONGOING;
                 }
                 if (codes.includes(ElvOAction.EXECUTION_EXCEPTION)) {
-                    this.ReportProgress("At least one item one exception occured while retrieving from Glacier");
+                    this.ReportProgress("At least one item one exception occurred while retrieving from Glacier");
                     return ElvOAction.EXECUTION_EXCEPTION;
                 }
                 this.ReportProgress("All files are available");
@@ -853,7 +842,7 @@ class ElvAwsS3Operation extends ElvOAction  {
         return info && info.details;
     };
     static TRACKER_THAWED = 53;
-    static VERSION = "0.2.8";
+    static VERSION = "0.2.7";
     static REVISION_HISTORY = {
         "0.0.1": "Initial release",
         "0.0.2": "Removed exessive logging",
@@ -873,8 +862,7 @@ class ElvAwsS3Operation extends ElvOAction  {
         "0.2.4": "Adds action to send back to glacier",
         "0.2.5": "Fixes status for attempting to send back to glacier files that are already frozen",
         "0.2.6": "Adds operation to create signed download link",
-        "0.2.7": "Adds option to list AWS S3 folder content",
-        "0.2.8": "Adds support for endpoint-url in download and upload operations"
+        "0.2.7": "Adds option to list AWS S3 folder content"
     };
 }
 
