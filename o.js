4ac0cbe1de3326b9703a37a186ac837419dad9a4
elv-joeA <joe.anakata@eluv.io>
2026-07-08 10:35:22 +0000

    package lock, and signal handler for better container

--- o.js
+++ o.js
@@ -1,6 +1,10 @@
 const logger = require('./o-logger');
 const ElvOCmd = require("./o-cmd");
 
+// for better stopping in container
+
+process.on('SIGINT',  () => process.exit(0))
+process.on('SIGTERM', () => process.exit(0))
 
 
 const Run = async function() {
